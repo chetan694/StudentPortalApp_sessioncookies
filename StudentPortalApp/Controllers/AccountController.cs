d74b0bbcfff08bffc3e42eb9aa462ca13b971796
Chetan Khajuria <chetankhajuria1493@gmail.com>
2026-05-09 21:39:28 +0530

    Add project files.

---
+++ StudentPortalApp/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortalApp.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+
+        // GET Login Page
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST Login Form
+        [HttpPost]
+        public IActionResult Login(string username)
+        {
+            HttpContext.Session.SetString("UserName", username);
+
+            return RedirectToAction("Dashboard");
+        }
+
+        public IActionResult Dashboard()
+        {
+            var user = HttpContext.Session.GetString("UserName");
+
+            ViewBag.User = user;
+
+            return View();
+        }
+
+     
+    }
+}
